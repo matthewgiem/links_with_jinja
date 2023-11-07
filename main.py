b4ad825c9f7800ee89e2995a7e23171531d2df2f
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-11-07 15:33:08 +0700

    print all the blog posts to the main screen

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+from post import Post
+import requests
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=post_objects)
+
+@app.route('/')
+def show_blog():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
